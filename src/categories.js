fbab15cf656048dbf0f4d00ab5da243643410f24
Abhishek jha <abhishek@talquo.com>
2022-08-24 22:42:26 +0545

    thela frontend

---
+++ src/categories.js
@@ -0,0 +1,17 @@
+const categories = [
+  {
+    name: "Combo pack",
+    img: "https://5.imimg.com/data5/BA/EF/GLADMIN-55089858/vegetable-combo-500x500.png",
+  },
+  {
+    name: "For a day ",
+    img: "https://organicfootprints.co.za/wp-content/uploads/2018/09/Essential_Veg_Co_580bb6d809a4b.jpg",
+  },
+
+  {
+    name: "Fruits",
+    img: "https://www.healthyeating.org/images/default-source/home-0.0/nutrition-topics-2.0/general-nutrition-wellness/2-2-2-3foodgroups_fruits_detailfeature.jpg?sfvrsn=64942d53_4",
+  },
+];
+
+export default categories;
